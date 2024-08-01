a22d28f94685d9e94137987b9c0b64597d654496
R Sri Sudarshan <srisudarshanrg@gmail.com>
2024-08-01 20:50:53 +0530

    basic solar analysis completed

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/srisudarshanrg/idp-project/pkg/config"
@@ -196,10 +198,103 @@ func (a *HandlerAccess) PostResourceProduction(w http.ResponseWriter, r *http.Re
 	})
 }
 
-func (a *HandlerAccess) SolarAnalysis(w http.ResponseWriter, r *http.Request) {
+func (a *HandlerAccess) Solar(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "solar-analysis.page.tmpl", &models.TemplateData{})
 }
 
-func (a *HandlerAccess) WindAnalysis(w http.ResponseWriter, r *http.Request) {
+func (a *HandlerAccess) PostSolar(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var requiredLandArea int
+
+	err = r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	requiredLandAreaRecieved := r.Form.Get("land_area")
+	requiredLandArea, err = strconv.Atoi(requiredLandAreaRecieved)
+
+	if err != nil && strings.Contains(err.Error(), "invalid syntax") {
+		errorMap := map[string]string{}
+		errorMap["integerValue"] = "You have to enter a positive value"
+		render.RenderTemplate(w, r, "solar-analysis.page.tmpl", &models.TemplateData{
+			Errors: errorMap,
+		})
+	}
+
+	getPlanQuery := `select * from solar where land_area_minimum <= $1`
+	rows, err := db.Query(getPlanQuery, requiredLandArea)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var plan, power, modules, batteries, accessories, electricity, company, link string
+	var id, land_area_minimum, land_area_maximum int
+
+	heading := "<h1 style='color: #fff; padding: 2%;'>Available Plans</h1>"
+
+	_, _ = fmt.Fprint(w, heading)
+
+	for rows.Next() {
+		err = rows.Scan(&id, &plan, &land_area_minimum, &land_area_maximum, &power, &modules, &batteries, &accessories, &electricity, &company, &link)
+		if err != nil {
+			log.Println(err)
+		}
+
+		land_area_minimum := strconv.Itoa(land_area_minimum)
+		land_area_maximum := strconv.Itoa(land_area_maximum)
+
+		html := `
+		    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+			<body style="background-color: rgb(20,20, 20); color:#fff;">
+				<table class="table table-striped table-dark">
+					<tr>
+						<td>Plan Name</td>
+						<td>Company</td>
+						<td>Minimum Area (sqft)</td>
+						<td>Maximum Area (sqft)</td>
+						<td>Power</td>
+						<td>Modules</td>
+						<td>Batteries</td>
+						<td>Accessories</td>
+						<td>Annual Electricity</td>
+						<td>Link</td>
+					</tr>
+
+					<tr>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td>%s</td>
+						<td><a href="%s">Visit Link</a></td>
+					</tr>
+				</table>
+			</body>
+		`
+
+		_, err := fmt.Fprintf(w, html, plan, company, land_area_minimum, land_area_maximum, power, modules, batteries, accessories, electricity, link)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	linkBack := `
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<br>
+		<a href='/solar' style='margin-left: 2%;' class='btn btn-primary'><i class="fa-solid fa-arrow-left"></i></a>
+
+		<script src="https://kit.fontawesome.com/2ce79bf423.js" crossorigin="anonymous"></script>
+	`
+
+	_, _ = fmt.Fprint(w, linkBack)
+	w.Header().Add("Content-Type", "text/html")
+}
+
+func (a *HandlerAccess) Wind(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "wind-analysis.page.tmpl", &models.TemplateData{})
 }
